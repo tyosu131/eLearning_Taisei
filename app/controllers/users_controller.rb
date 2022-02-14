3c72246cd28870ffbe083533f0d0a8b79dcb8da9
tyosu131 <taisei514@icloud.com>
2022-02-14 20:06:52 +0900

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  # def new
-  #   @user = User.new(name: params[:name], email: params[:email])
-  # end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
@@ -13,4 +13,15 @@ class UsersController < ApplicationController
    end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 12)
+    @micropost = current_user.microposts.build if logged_in?
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end

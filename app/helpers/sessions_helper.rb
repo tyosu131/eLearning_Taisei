241268bc09e061c04aa2b7441a6c5bcb10b0db75
tyosu131 <taisei514@icloud.com>
2022-02-15 23:24:50 +0900

    fixed code based on comments

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,28 +1,28 @@
 module SessionsHelper
-    def log_in(user)
-      session[:user_id] = user.id
-    end
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-    def current_user
-      User.find_by(id: session[:user_id])
-    end
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 
-    def logged_in?
-      !current_user.nil?
-    end
+  def logged_in?
+    !current_user.nil?
+  end
 
-    def log_out
-      session.delete(:user_id)
-    end
+  def log_out
+    session.delete(:user_id)
+  end
 
-    # def logged_in_user
-    #     unless logged_in?
-    #         flash[:info] = "Please login first."
-    #         redirect_to login_url
-    #     end
-    # end
+  # def logged_in_user
+  #     unless logged_in?
+  #         flash[:info] = "Please login first."
+  #         redirect_to login_url
+  #     end
+  # end
 
-    # def current_user?(user)
-    #     return true if user == current_user
-    # end
+  # def current_user?(user)
+  #     return true if user == current_user
+  # end
 end

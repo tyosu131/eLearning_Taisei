5f81d90f36c7bbec0eab44146154dc4bca8ecfea
Taisei <95160728+tyosu131@users.noreply.github.com>
2022-02-21 23:04:36 +0900

    Created following_function. (#7)
    * Created following_function.
    
    * Fixed code based on comments
    
    * Deleted microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts
   validates :name, presence: true, length: { maximum: 50}
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 50},
@@ -6,4 +7,28 @@ class User < ApplicationRecord
                                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  has_many :active_relationships, foreign_key: "follower_id",
+                                class_name: "Relationship",
+                                dependent: :destroy
+
+  has_many :followed_users, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, foreign_key: "followed_id",
+                                     class_name: "Relationship",
+                                     dependent: :destroy
+
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end

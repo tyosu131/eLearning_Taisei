572e34065ff06c4da5ac18ef41540e6161cbab66
Taisei <95160728+tyosu131@users.noreply.github.com>
2022-02-28 22:27:21 +0900

    Create admin_category_CRUD (#9)
    * Create admin_category_CRUD
    
    * I fixed show page based on your comments.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,22 +2,50 @@ class Admin::CategoriesController < ApplicationController
   before_action :only_admin
 
   def new
-    @user = User.new
+    @category = Category.new
   end
 
   def create
-    
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Success! New category created."
+      redirect_to admin_categories_path
+    else
+      flash[:danger] = "This form contains errors."
+      render 'new'
+    end
   end
 
   def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category Update"
+      redirect_to admin_categories_path
+    else
+      render 'edit'
+    end
   end
 
   def index
-    @users = User.all.order("created_at DESC")
+    @categories = Category.paginate(page: params[:page], per_page: 10)
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path
   end
 
   private
-  # def if_not_admin
-  #   redirect_to root_path unless current_user.admin?
-  # end
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_114753) do
+ActiveRecord::Schema.define(version: 2022_02_22_140131) do
 
-  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.integer "user_id"
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

6eb260ded29169dfb7dbdf47827bc808907bc347
tyosu131 <taisei514@icloud.com>
2022-03-03 22:33:11 +0900

    choices task

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,6 @@
 class Word < ApplicationRecord
   belongs_to :category
+  has_many :choices, dependent: :destroy
   validates :word, presence: true, length: { minimum: 1 }
+  accepts_nested_attributes_for :choices
 end
--- db/migrate/20220302133605_create_choices.rb
+++ db/migrate/20220302133605_create_choices.rb
@@ -1,6 +1,9 @@
 class CreateChoices < ActiveRecord::Migration[6.0]
   def change
     create_table :choices do |t|
+      t.string :choice, null: false
+      t.boolean :is_answer
+      t.integer :word_id
 
       t.timestamps
     end

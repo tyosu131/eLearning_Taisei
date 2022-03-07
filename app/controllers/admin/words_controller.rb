70fb39d89b923fcb5f265c19e9f2785cd56968f4
tyosu131 <taisei514@icloud.com>
2022-03-07 20:05:26 +0900

    Created choices

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -29,8 +29,7 @@ class Admin::WordsController < ApplicationController
   def update
     @category = Category.find(params[:category_id])
     @word = Word.find(params[:id])
-    if @word.update_attributes!(word_params) 
-      abort
+    if @word.update_attributes(word_params) 
       flash[:success] = "Updated"
       redirect_to admin_category_path(@category)
     else

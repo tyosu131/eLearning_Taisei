6c65a95bbf2463d6cabe89f03ec15e3e6e65f9e3
tyosu131 <taisei514@icloud.com>
2022-03-11 21:37:19 +0900

    Created lessons page

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,9 @@
 module LessonsHelper
+  def is_done(category)
+    Lesson.find_by(category_id: category.id, user_id: current_user.id)
+  end   
+
+  def get_result(category)
+    Lesson.find_by(category_id: category.id, user_id: current_user.id).result
+  end
 end
